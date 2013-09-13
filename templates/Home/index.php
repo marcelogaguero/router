e4a23cd1305de7a99319598dd0bbc30e9750f299
marcelo <marcelogaguero@hotmail.com>
2013-09-13 14:56:14 -0300

    template

--- templates/Home/index.php
+++ templates/Home/index.php
@@ -1,1 +1,10 @@
-<?php echo "Es una prueba"; ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $this->config['Project']['title']; ?></title>
+</head>
+<body>
+    <?php echo "Es una prueba"; ?>
+</body>
+</html>
